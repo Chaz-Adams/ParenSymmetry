456a2b99b67e7bd886a385cd9c466efc36532d7a
Chaz-Adams <caadams1922@gmail.com>
2024-03-08 08:15:04 -0500

    finished

--- src/main/java/src/main/java/ParenSymmetry.java
+++ src/main/java/src/main/java/ParenSymmetry.java
@@ -7,17 +7,38 @@ import java.io.IOException;
 public class ParenSymmetry {
 
 
-    private Boolean isBalanced(String s) {
-        // implement this method
-        return null;
+    public Boolean isBalanced(String s) {
+        char openBrace = '(';
+        int openBraceCount = 0;
+        char closingBrace = ')';
+        int closingBraceCount = 0;
+
+        char[] charArray = s.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+            if(charArray[i] == openBrace){
+                openBraceCount++;
+            }else if(charArray[i] == closingBrace){
+                closingBraceCount++;
+            }
+        }
+        if(openBraceCount == closingBraceCount && openBraceCount > 0){
+//            System.out.println("Open brace: "+openBraceCount+"\nClosed Brace: "+closingBraceCount);
+//            System.out.println("RETURNING TRUE");
+            return true;
+        }else
+//            System.out.println("Open brace: "+openBraceCount+"\nClosed Brace: "+closingBraceCount);
+//            System.out.println("RETURNING FALSE");
+            return false;
+
     }
 
     private static void checkFile(String filename) throws IOException {
         // open file named filename
-       BufferedReader br = new BufferedReader(new FileReader("TestStrings0.txt"));
+       BufferedReader br = new BufferedReader(new FileReader(filename));
        String line;// for each line in the file
        while((line = br.readLine()) != null){ // read the line
-           System.out.println(br); // print whether or not the line's parenthesis are balanced
+           System.out.println(line); // print whether the line's parenthesis are balanced
        }
         br.close(); // CLOSE the file
     }
